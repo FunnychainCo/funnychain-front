e2370d377a22953f3fd266095982b6a367d69ce2
Pierre-Henri <pierreh37@gmail.com>
2018-04-09 16:33:09 +0200

    list firebase db

---
+++ src/components/MemeList.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import firebase from 'firebase';
+import {Card, CardHeader, CardMedia, CardTitle} from "material-ui";
+
+export default class MemeList extends Component {
+    memeDataBase = "memes"
+    imageDataBase = "images"
+    userDataBase = "users"
+    state = {
+        memes: {}
+    }
+
+    componentDidMount() {
+        var ref = firebase.database().ref(this.memeDataBase);
+        ref.on("value", (memes) => {
+            var memesValue = memes.val();
+            var itemsKeys = Object.keys(memesValue);
+            itemsKeys.forEach((key) => {
+                var meme = memesValue[key];
+                //load image data
+                firebase.database().ref(this.imageDataBase+"/"+meme.iid).on("value", (image) => {
+                    var imageValue = image.val();
+                    //load user data
+                    firebase.database().ref(this.userDataBase+"/"+meme.uid).on("value", (user) => {
+                        var userValue = user.val();
+                        //update the state
+                        this.state.memes[key] = meme;
+                        this.state.memes[key].user = userValue;
+                        this.state.memes[key].image = imageValue;
+                        this.forceUpdate();
+                    });
+                });
+            })
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    Object.keys(this.state.memes).map((key) => {
+                        return <Card>
+                                <CardHeader title={this.state.memes[key].user.email}/>
+                                <CardMedia overlay={<CardTitle title={this.state.memes[key].title}/>}>
+                                    <img src={this.state.memes[key].image.url} alt="" />
+                                </CardMedia>
+                            </Card>
+                    })
+                }
+            </div>);
+    }
+}
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import MemeList from "../components/MemeList";
 
 export default class Home extends Component {
   render () {
     return (
       <div>
-        Home.
+        <MemeList/>
       </div>
     )
   }
