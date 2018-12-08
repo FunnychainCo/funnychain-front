da4b28e7850d33bec2a1ad22a2b7737648443c5b
Pierre-Henri <pierreh37@gmail.com>
2018-12-08 17:25:39 +0100

    update tag version

--- docker/tag_version.bat
+++ docker/tag_version.bat
@@ -1,3 +1,3 @@
 docker login
-docker tag registry.admin.rphstudio.net/funnychain:latest registry.admin.rphstudio.net/funnychain:0.2.15
-docker push registry.admin.rphstudio.net/funnychain:0.2.15
+docker tag registry.admin.rphstudio.net/funnychain:latest registry.admin.rphstudio.net/funnychain:1.2.8
+docker push registry.admin.rphstudio.net/funnychain:1.2.8
