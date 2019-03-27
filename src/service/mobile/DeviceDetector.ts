0ef1736a9793191b2de2f33e4785e9c89fe47d07
Pierre-Henri <pierreh37@gmail.com>
2019-03-27 11:28:29 +0100

    Add support for iphone XR and XS and max

--- src/service/mobile/DeviceDetector.ts
+++ src/service/mobile/DeviceDetector.ts
@@ -33,8 +33,20 @@ export class DeviceDetector {
          Browser Dimensions: 980 x 1956 (pixels)
          Screen Resolution: 375 x 812 (pixels)
          Browser Dimensions: 980 x 448 (pixels)
+
+         iPhone XS Max	1242px × 2688px	2688px × 1242px => 414 x 896
+         iPhone XS	1125px × 2436px	2436px × 1125px => 375 x 812
+         iPhone XR	828px × 1792px	1792px × 828px => 276 x 597 ou 598
+         iPhone X	1125px × 2436px	2436px × 1125px
+
+         //https://developer.apple.com/library/archive/documentation/DeviceInformation/Reference/iOSDeviceCompatibility/Displays/Displays.html
+         1125 x 2436         375 x 812         3.0         3.0
          * */
-        return this.isMobile() && this.isIPhone() && (screen.width==375 && screen.height==812)
+        return this.isMobile() && this.isIPhone() && (
+            (screen.width==375 && screen.height==812) || //Iphone X Iphone XS
+            (screen.width==375 && (screen.height==597 || screen.height==598)) || //iPhone XR
+            (screen.width==414 && screen.height==896)    //iPhone XS Max
+        )
     }
 
     getDeviceString():string{
